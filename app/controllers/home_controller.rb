db3f4fe97f2c6a738e157e06e37ff17357c99061
Aman Singh <aman.webdragon@gmail.com>
2015-10-10 13:40:55 -0400

    added user profile/passwd edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @public_event = Event.where(access_type:"public");
-    @private_event = Event.where(access_type:"private");
-    @closed_event = Event.where(access_type:"closed");
+    @public_event = Event.order_by(datetime: 'asc')#where(access_type:"public");
+    # @private_event = Event.where(access_type:"private");
+    # @closed_event = Event.where(access_type:"closed");
   end
 end
