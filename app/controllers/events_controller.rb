e9089e8f5399accebab58953e822c4008ca2ffa1
rakeshdrk <rakeshdrk29@gmail.com>
2015-10-11 22:45:53 -0400

    event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,43 @@
 class EventsController < ApplicationController
    def index
-
+     @events = Event.all	
    end
 
    def new
-
+     @event = Event.new
    end
-
+   
+   def edit 
+     @event = Event.find(params[:id])
+   end
+  
    def show
      @event = Event.find(params[:id])	
    end
 
    def create
      @event = Event.new(params[:event].permit(:title,:access_type,:description,:location,:datetime,:link))
-     @event.save
-     redirect_to @event
+     if @event.save
+       redirect_to @event
+     else
+       render 'new' 
+     end
    end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update((params[:event].permit(:title,:access_type,:description,:location,:datetime,:link)))
+     redirect_to @event
+    else
+     render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+ 
+    redirect_to events_path
+  end
 end
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,11 @@
 class Event
   include Mongoid::Document
   include Mongoid::Timestamps
-
   field :title, type: String
   field :datetime, type: String
   field :description, type: String
   field :location, type: String
   field :link, type: String
   field :access_type, type: String
-
+  validates :title,:datetime,:description,:location,:access_type,presence: true
 end
