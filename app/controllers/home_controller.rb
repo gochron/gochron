6faea50abb2a97e240a7c81b50acadc14d58cfbc
shantanukande <shaansaheb@gmail.com>
2015-10-11 23:21:51 -0400

    calendar is inserted

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,8 @@ class HomeController < ApplicationController
     @public_event = Event.order_by(datetime: 'asc')#where(access_type:"public");
     # @private_event = Event.where(access_type:"private");
     # @closed_event = Event.where(access_type:"closed");
+    @event = Event.all;
+    @event_by_date = @event.group_by(&:datetime);
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today;
   end
 end
