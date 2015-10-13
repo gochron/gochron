bb1c800e59cf1aff97c29091186f0b2d965aae77
akshithaainapuri <akshithaa92@gmail.com>
2015-10-13 14:25:13 -0400

    Group create/Delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,29 @@
 class GroupsController < ApplicationController
   def index
-
+     @group = Group.all
   end
 
   def new
 
+  end
+
+   def show
+     @group = Group.find(params[:id])	
+   end
+
+  def create
+     @group = Group.new(params[:group].permit(:name,:description))
+     if @group.save
+     redirect_to @group
+     else
+       render 'new' 
+     end
+   end
+
+    def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+ 
+    redirect_to groups_path
   end
 end
