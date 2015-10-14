fac70acd3e85bd55d60fda889a6d316ec7676ed7
shantanukande <shaansaheb@gmail.com>
2015-10-13 23:09:29 -0400

    show events in tiles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,17 @@ class HomeController < ApplicationController
     # @private_event = Event.where(access_type:"private");
     # @closed_event = Event.where(access_type:"closed");
     @event = Event.all;
-    @event_by_date = @event.group_by(&:datetime);
+    @event_by_date = {} # { "2015-10-16" => [ Event, Event, Event ], "2015-10-17" => [Event] ... }
+    @event.each do |e|
+      d = e.datetime.to_date.to_s
+      if @event_by_date[d].blank?
+        @event_by_date[d] = [e]
+      else
+        @event_by_date[d] << e
+      end
+    end
+
+    # @event_by_date = @event.group_by(&:datetime);
     @date = params[:date] ? Date.parse(params[:date]) : Date.today;
   end
 end
